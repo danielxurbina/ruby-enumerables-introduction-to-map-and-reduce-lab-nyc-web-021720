961cb7ccbd3ffe39b1efa86315e6211dadb540dc
Daniel Urbina <danielur1999@gmail.com>
2020-02-13 07:54:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,3 +35,15 @@ while i < source_array.length do
 end
 array
 end 
+
+def reduce_to_total(source_array, starting_point)
+
+end 
+
+def reduce_to_all_true(source_array)
+
+end 
+
+def reduce_to_any_true(source_array)
+
+end 
