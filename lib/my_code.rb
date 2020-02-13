f848a00f2ff537a1837484986da9b6bd28fd825d
Daniel Urbina <danielur1999@gmail.com>
2020-02-13 08:01:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -36,8 +36,9 @@ end
 array
 end 
 
-def reduce_to_total(source_array, starting_point = 0)
+def reduce_to_total(source_array, starting_point)
 i = 0
+starting_point = 0
 sum = starting_point
 while i < source_array.length do
   sum = source_array[i] + sum
