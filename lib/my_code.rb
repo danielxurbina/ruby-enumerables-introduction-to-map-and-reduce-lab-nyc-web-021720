baf05e6e9ab35d42a21fdcde5c918b002f1189bf
Daniel Urbina <danielur1999@gmail.com>
2020-02-13 07:48:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,13 @@
 # My Code here....
 def map_to_negativize(source_array)
-
+array = []
+i = 0 
+while i < source_array.length do
+  current_index = source_array[i]
+  array << source_array[i] * -1
+  i += 1
+end
+array 
 end
 
 def map_to_no_change(source_array)
