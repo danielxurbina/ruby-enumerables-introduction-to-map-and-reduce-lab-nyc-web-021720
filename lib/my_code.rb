411079d03a06face32dfcfe3ffee6809a061b6af
Daniel Urbina <danielur1999@gmail.com>
2020-02-13 08:04:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,7 +49,7 @@ end
 def reduce_to_all_true(source_array)
 i = 0 
 while i < source_array.length do 
-  if source_array[i] != Truthy
+  return source_array[i]
 end 
 
 def reduce_to_any_true(source_array)
