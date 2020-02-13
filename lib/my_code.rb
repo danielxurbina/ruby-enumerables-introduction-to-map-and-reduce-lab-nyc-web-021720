f6f87a5c8cbc573bed631435e4598580c54bf61e
Daniel Urbina <danielur1999@gmail.com>
2020-02-13 07:51:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,7 +15,14 @@ source_array
 end 
 
 def map_to_double(source_array)
-
+array = []
+i = 0
+while i < source_array.length do
+  current_index = source_array[i]
+  array << source_array[i] * 2
+  i += 1
+end
+array
 end 
 
 def map_to_square(source_array)
